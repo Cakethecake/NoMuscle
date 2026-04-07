729ee8ba1d7f7d9e1c7888744bd8a73214184969
Cakethecake <1cakethecake1@gmail.com>
2026-04-07 15:40:34 -0400

    Updated Event and Room

--- src/main/java/NoMuscle/Event.java
+++ src/main/java/NoMuscle/Event.java
@@ -1,4 +1,17 @@
 package NoMuscle;
 
 public class Event {
+
+    private double probCoef;
+    public Entity containedFight;
+    public Occurence containedOcc;
+
+    Event() {
+        probCoef = 1;
+        containedFight = new Entity();
+        containedOcc = new Occurence();
+
+    }
+
+
 }
--- src/main/java/NoMuscle/Room.java
+++ src/main/java/NoMuscle/Room.java
@@ -1,4 +1,23 @@
 package NoMuscle;
 
 public class Room {
+
+    public String name;
+    public Event[] eventsInside;
+    public double[] eventProbs;
+
+    Room() {
+        name = "Test";
+        eventsInside = new Event[3];
+        eventProbs = new double[] {20, 10, 70};
+
+    }
+
+    Room(String name) {
+
+        this.name = name;
+
+
+
+    }
 }
