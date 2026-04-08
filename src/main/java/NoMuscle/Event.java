9b45486721094ffd3961d008bade16ae906709f5
Jax !! <nightwolfdajodie@gmail.com>
2026-04-08 15:37:13 -0400

    Update Event.java

--- src/main/java/NoMuscle/Event.java
+++ src/main/java/NoMuscle/Event.java
@@ -1,11 +1,25 @@
 package NoMuscle;
 
 public class Event {
-    Event thisEvent;
+    Event thisEvent; // idk what this is
+    private int probCoeficient;
+    public Item getItem;
+
+    /**
+     * Event class with no parameters - normal events are 60%
+     */
+    public Event() {
+        this.probCoeficient = 60;
+    }
+
+    public int getProbCoeficient() {
+        return probCoeficient;
+    }
+
 
     public void applyEvent(){
         thisEvent.doThing();
-    }
+    } // this too
 
     public void doThing() {
     }
