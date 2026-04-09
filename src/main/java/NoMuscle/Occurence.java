1cb26485377cabaf965c1d2f409b9330cb1bf050
Caique <zamanc@vcu.edu>
2026-04-09 18:35:08 -0400

    main file

--- src/main/java/NoMuscle/Occurence.java
+++ src/main/java/NoMuscle/Occurence.java
@@ -1,14 +1,36 @@
 package NoMuscle;
 
 public class Occurence extends Event {
+
     protected String name;
 
     public Occurence(String name) {
+
         this.name =name;
     }
 
+    public boolean canGive(Player player) {
+        return true
+    }
+
+
+    public void extractCost(Player player) {
+
+    }
+
+    public void giveReward(Player player) {
+
+    }
 
     public void doThing(Player player) {
-        super.doThing();
+        System.out.println("Encounter:" + name);
+
+        if (canGive(player)) {
+            extractCost(player);
+            giveReward(player);
+
+        } else {
+            System.out.println("Cannot afford");
+        }
     }
 }
